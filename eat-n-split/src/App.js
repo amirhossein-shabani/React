09d607fcafdb5979b4ff160a9cbbe13e82550059
amsein <shabanishabaniamirhossein98@gmail.com>
2025-03-05 18:59:05 +0330

    Use Effect done! &&  usePopcorn project done!

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -108,6 +108,7 @@ export default function App() {
           <FormSplitBill
             selectedFriend={selectedFriend}
             onSplitBill={handleSplitBill}
+            key={selectedFriend.id}
           />
         )}
       </div>
--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import React from "react";
+import "./index.css";
+import App from "./App";
 
 import StarRating from "./StarRating";
 
-function Test() {
-  const [moveRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [moveRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movies was rated {moveRating} stars </p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movies was rated {moveRating} stars </p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
@@ -32,6 +32,6 @@ root.render(
       defaultRating={3}
     />
 
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
